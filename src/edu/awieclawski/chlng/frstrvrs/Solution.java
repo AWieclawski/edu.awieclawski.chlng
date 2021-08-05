15165fa1b881af5e618d64847c98cd3617057a68
AWieclawski <a_wieclawski@poczta.pl>
2021-08-05 14:07:18 +0200

    First Reverse

---
+++ src/edu/awieclawski/chlng/frstrvrs/Solution.java
@@ -0,0 +1,40 @@
+package edu.awieclawski.chlng.frstrvrs;
+
+/**
+ * CODERBYTE
+ * 
+ * First Reverse
+ * 
+ * Have the function FirstReverse(str) take the str parameter being passed and
+ * return the string in reversed order. For example: if the input string is
+ * "Hello World and Coders" then your program should return the string sredoC
+ * dna dlroW olleH.
+ * 
+ * Examples
+ * 
+ * Input: "coderbyte" Output: etybredoc
+ * 
+ * Input: "I Love Code" Output: edoC evoL I
+ *
+ */
+public class Solution {
+
+	public static String FirstReverse(String str) {
+
+//		return new StringBuilder().append(str).reverse().toString();
+		return new StringBuffer().append(str).reverse().toString();
+	}
+
+	public static void main(String[] args) {
+
+		String str = "";
+
+		str = "coderbyte";
+		System.out.println(FirstReverse(str));
+
+		str = "I Love Code";
+		System.out.println(FirstReverse(str));
+
+	}
+
+}

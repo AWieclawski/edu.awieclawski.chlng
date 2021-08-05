1123440ec07c022280336043bf7116c7bf87de76
AWieclawski <a_wieclawski@poczta.pl>
2021-08-05 10:42:01 +0200

    Add LongestWord,FirstFactorial

---
+++ src/edu/awieclawski/chlng/frstfctrl/Solution.java
@@ -0,0 +1,57 @@
+package edu.awieclawski.chlng.frstfctrl;
+
+/**
+ * 
+ * CODERBYTE
+ * 
+ * First Factorial
+ * 
+ * Have the function FirstFactorial(num) take the num parameter being passed and
+ * return the factorial of it. For example: if num = 4, then your program should
+ * return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1
+ * and 18 and the input will always be an integer.
+ * 
+ * Examples
+ * 
+ * Input: 4 Output: 24
+ * 
+ * Input: 8 Output: 40320
+ * 
+ * Input: 7 Output: 5040
+ *
+ */
+public class Solution {
+
+	public static int FirstFactorial(int num) {
+		int fact = 1;
+
+		for (int i = 2; i <= num; i++)
+			fact *= i;
+
+		return fact;
+	}
+
+	public static void main(String[] args) {
+		int num = 0;
+
+		num = 3;
+		System.out.println(FirstFactorial(num));
+
+		num = 4;
+		System.out.println(FirstFactorial(num));
+
+		num = 7;
+		System.out.println(FirstFactorial(num));
+
+		num = 8;
+		System.out.println(FirstFactorial(num));
+
+		num = 9;
+		System.out.println(FirstFactorial(num));
+		
+		num = 10;
+		System.out.println(FirstFactorial(num));
+
+	}
+
+}
---
+++ src/edu/awieclawski/chlng/lngstwrd/Solution.java
@@ -0,0 +1,59 @@
+package edu.awieclawski.chlng.lngstwrd;
+
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * CODERBYTE
+ * 
+ * Longest Word
+ * 
+ * Have the function LongestWord(sen) take the sen parameter being passed and
+ * return the longest word in the string. If there are two or more words that
+ * are the same length, return the first word from the string with that length.
+ * Ignore punctuation and assume sen will not be empty. Words may also contain
+ * numbers, for example "Hello world123 567"
+ * 
+ * Examples
+ * 
+ * Input: "fun&!! time" Output: time
+ * 
+ * Input: "I love dogs" Output: love
+ *
+ */
+public class Solution {
+
+	public static String LongestWord(String sen) {
+		String longest = "";
+		int lngth = -1;
+		String regex = "^[a-zA-Z0-9]+$";
+		String[] splited = sen.split("\\s+");
+//		System.out.println(" -- " + Arrays.toString(splited));
+
+		for (String word : splited) {
+			if (Pattern.matches(regex, word))
+				if (lngth < word.length()) { // then only first found if equals length
+					lngth = word.length();
+					longest = word;
+				}
+		}
+
+		return longest;
+	}
+
+	public static void main(String[] args) {
+
+		String sen = "";
+
+		sen = "fun&!! time";
+		System.out.println(LongestWord(sen));
+
+		sen = "I love abds345  dogs";
+		System.out.println(LongestWord(sen));
+
+		sen = "I love 123.,ft  dogs for  ever";
+		System.out.println(LongestWord(sen));
+
+	}
+
+}
